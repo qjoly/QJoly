e5f710a3b6a4b2c2209650d3794d5b5b8702f779
Quentin JOLY <82603435+QJoly@users.noreply.github.com>
2022-10-15 07:44:54 +0200

    Update progress.js

--- progress.js
+++ progress.js
@@ -14,6 +14,34 @@ function generateProgressBar() {
 }
 
 const readme = `\
+<h2 align="center">👋 Hello!</h2>
+<p align="center">
+  <a href="https://thebidouilleur.xyz">Blog</a> •
+  <a href="https://twitter.com/thebidouilleur">Twitter</a>
+</p>
+
+
+- 🔭 I’m currently working in [French government](https://www.gouvernement.fr/).
+- 🌱 I’m currently learning more on Nix and better [X-as-code](https://quadralogics.com/research/XAsCode.html) practices.
+- 💬 Ask me about **Containers, Kubernetes and Python**
+- 📫 How to reach me: [@TheBidouilleur](https://twitter.com/TheBidouilleur) on Twitter
+
+![Kubernetes](https://img.shields.io/badge/kubernetes-%23326ce5.svg?style=for-the-badge&logo=kubernetes&logoColor=white) ![Terraform](https://img.shields.io/badge/terraform-%235835CC.svg?style=for-the-badge&logo=terraform&logoColor=white) ![Packer](https://img.shields.io/badge/packer-%23E7EEF0.svg?style=for-the-badge&logo=packer&logoColor=%2302A8EF) ![Ansible](https://img.shields.io/badge/ansible-%231A1918.svg?style=for-the-badge&logo=ansible&logoColor=white) ![NixOS](https://img.shields.io/badge/NixOS-48B9C7?style=for-the-badge&logo=NixOS&logoColor=white)
+
+ -------
+
+**📝 Latest Blog Posts**
+
+<!-- BLOG-POST-LIST:START -->
+- [Remarkable, une avancée dans les prises de notes](https://thebidouilleur.xyz/blog/remarkable)
+- [NixOS, Ma nouvelle distribution](https://thebidouilleur.xyz/blog/nixos)
+- [Mes débuts à la gyroroue](https://thebidouilleur.xyz/blog/gyroroue)
+- [Longhorn, stockage distribué](https://thebidouilleur.xyz/blog/longhorn)
+- [Mes débuts avec s3](https://thebidouilleur.xyz/blog/s3contabo)
+<!-- BLOG-POST-LIST:END -->
+
+-------
+
 ### Year progress
 
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
