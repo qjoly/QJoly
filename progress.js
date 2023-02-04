01d09566650c014d2b41b7cbf1b52bdc12d357a1
Quentin JOLY <82603435+QJoly@users.noreply.github.com>
2023-02-04 23:45:36 +0100

    Update progress.js

--- progress.js
+++ progress.js
@@ -53,7 +53,7 @@ const readme = `\
 <br>
 
 ### Other
-
+<script defer data-domain="github.com" src="https://stats.192168128.xyz/js/plausible.js"></script>
 <img align="center" src="https://visitor-badge.glitch.me/badge?page_id=qjoly.qjoly" alt="thebidouilleur" />
 
 
